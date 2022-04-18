5cf123a13097d30383c8eebc55971677bb143c7b
AJ <abhandal@umd.edu>
2022-04-18 11:50:26 +1000

    Initial Commit

---
+++ src/main/java/com/bhandal/springmicroservicebeerclient/SpringMicroserviceBeerClientApplication.java
@@ -0,0 +1,13 @@
+package com.bhandal.springmicroservicebeerclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroserviceBeerClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMicroserviceBeerClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bhandal/springmicroservicebeerclient/SpringMicroserviceBeerClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhandal.springmicroservicebeerclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroserviceBeerClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
