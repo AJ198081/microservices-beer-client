f9a7acb212fdb59ecc3d00f939688d32222ed134
AJ <abhandal@umd.edu>
2022-04-18 13:25:07 +1000

    Set and Test the RestTemplate

---
+++ src/test/java/com/bhandal/springmicroservicebeerclient/web/client/BreweryClientTest.java
@@ -0,0 +1,42 @@
+package com.bhandal.springmicroservicebeerclient.web.client;
+
+import com.bhandal.springmicroservicebeerclient.web.model.BeerDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.net.URI;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class BreweryClientTest {
+
+    @Autowired
+    BreweryClient breweryClient;
+
+    @Test
+    void getBeerById() {
+        BeerDto dto = breweryClient.getBeerById(UUID.randomUUID());
+        assertNotNull(dto);
+    }
+
+    @Test
+    void testSaveNewBeer() {
+        BeerDto beerDto = BeerDto.builder()
+                .beerName("Corona")
+                .build();
+        final URI uri = breweryClient.saveNewBeer(beerDto);
+        assertNotNull(uri);
+        System.out.println(uri);
+    }
+
+    @Test
+    void updateBeer() {
+        BeerDto beerDto = BeerDto.builder()
+                .beerName("Corona")
+                .build();
+        breweryClient.updateBeer(UUID.randomUUID(), beerDto);
+    }
+}
